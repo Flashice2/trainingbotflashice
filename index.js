539985440965e0ec42c95cbceccff8ffe4115f6b
flash1ce <66264110+flash1ce@users.noreply.github.com>
2020-06-01 20:46:29 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,10 +9,28 @@ bot.on('ready', function () {
 
 bot.on("message", msg => {
     if (msg.content === "!help") {
-        const commande = "!jambes \n !fessier !bras !abdos !dos !pect"
+        const commande = "\n!jambes : ******* \n!fessier : ******* \n!bras : ******* \n!abdos : ******* \n!dos : ******* \n!pect : *******"
         msg.reply(commande);
     }
-    // les commande
+    if (msg.content === "!jambes") {
+        msg.reply("jambe");
+    }
+    if (msg.content === "!fessier") {
+        msg.reply("fessier");
+    }
+    if (msg.content === "!bras") {
+        msg.reply("bras");
+    }
+    if (msg.content === "!abdos") {
+        msg.reply("abdos");
+    }
+    if (msg.content === "!dos") {
+        msg.reply("dos");
+    }
+    if (msg.content === "!pect") {
+        msg.reply("pect");
+    }
+
 });
 
 
